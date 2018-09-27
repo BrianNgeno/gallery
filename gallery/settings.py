7779964e7e2b98b34f083f227aa903f12fcc8839
brianngeno <bkn.ngeno@gmail.com>
2018-09-27 23:10:54 +0300

    defined botstrap and app on the settings in project

--- gallery/settings.py
+++ gallery/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'galla',
+    'bootstrap3',
     'galla.apps.GallaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
