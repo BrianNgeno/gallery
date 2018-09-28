139e2f06a12778afdd106e2afe474b565790fa91
brianngeno <bkn.ngeno@gmail.com>
2018-09-28 16:44:42 +0300

    updated the admin page to pass many to many fields

--- galla/admin.py
+++ galla/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import Editor,Categorys,Image,Location
+
+
 # Register your models here.
+
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal = ('Categorys')
+    
 admin.site.register(Editor)
 admin.site.register(Categorys)
-admin.site.register(Image)
+admin.site.register(Image,ImageAdmin)
 admin.site.register(Location)
