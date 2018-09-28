10c07876c5d2054c0bf32f7976476160b5a35852
brianngeno <bkn.ngeno@gmail.com>
2018-09-28 18:59:22 +0300

    added instance test of editor

--- galla/tests.py
+++ galla/tests.py
@@ -7,3 +7,7 @@ class EditorTestClass(TestCase):
     # Set up method
     def setUp(self):
         self.brian= Editor(first_name = 'Brian', last_name ='Ngeno', email ='bkn.ngeno@gmail.com')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.brian,Editor))
