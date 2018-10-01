bac770160a5ad6b229320d66384267c0c1a47bf3
brianngeno <bkn.ngeno@gmail.com>
2018-10-01 06:58:02 +0300

    adding copy paste suceess

--- galla/static/js/basegalla.js
+++
@@ -1,8 +0,0 @@
-function copyLink() {
-    var copyText = document.getElementById("thislinkhere");
-    copyText.select();
-    document.execCommand("copy");
-  
-    /* Alert the copied text */
-    alert("Text Copied : " + copyText.value);
-  }
